2275ee3116eaf98e4e03ab448e281d19b280e2f3
Coder2411 <98026546+Coder2411@users.noreply.github.com>
2026-08-11 01:23:42 +0530

    Add files via upload

---
+++ learning.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const double pi = 3.14;
+    const int n = 5;
+
+    //pi = 6;
+
+    cout << pi << endl;
+    cout << n << endl;
+
+    return 0;
+}
