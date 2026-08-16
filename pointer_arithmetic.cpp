4ade1b860fd6295da9d1cdd2e03e7b10e2341d62
Coder2411 <98026546+Coder2411@users.noreply.github.com>
2026-08-17 01:25:27 +0530

    Update pointer_arithmetic.cpp

--- pointer_arithmetic.cpp
+++ pointer_arithmetic.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 int main() {
     int arr[] = {62, 71, 72, 74, 88};
+    int* ptr = arr; // Points to arr[0]
     int n = sizeof(arr)/sizeof(arr[0]);
+
+    for (int i = 0; i < n; i++) {
+        cout << *ptr << endl;
+        ptr++;
+    }
+
+    cout << "\n";
 
     for (int i = 0; i < n; i++) {
         cout << *(arr+i) << endl;
